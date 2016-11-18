df2c4a2b60f83288e5b8bd3d40eaf3034e8c9254
Nitai Charan <nitaicharan@gmail.com>
2016-11-18 17:28:03 -0300

    Teste FILE

--- Main.c
+++ Main.c
@@ -1,6 +1,17 @@
 #include"head.h"
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    int c=getc(stdin);
+    FILE *open = fopen("nitai","r");
+    do {
+        do {
+            printf("%c",c);
+        } while(c != EOF && c!=',');
+        printf(",");
+        if(c!=EOF)fgetc(open);
+    } while(c!=EOF);
+    printf("\n");
+    fclose(open);
+
     return 0;
 }
